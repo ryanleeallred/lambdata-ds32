dcdf39ebe72051b26003254d17332f65dde53a97
Ryan Allred <ryanleeallred@gmail.com>
2021-10-25 12:05:43 -0600

    adds lambdata package and helper_function.py file

---
+++ lambdata/helper_functions.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+def null_count(df):
+    '''Returns the number of null values 
+    across the entire dataframe'''
+    return df.isnull().sum().sum()
+
+print(null_count(pd.DataFrame({'a': [1,2,np.NaN], 'b':[np.NaN, np.NaN, 6]})))
